205b90564ed812c549e50011ccfb38168b2c7f30
Hugo <gitjohan5@gmail.com>
2024-05-22 13:56:20 +0300

    background & files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-3xl font-bold">Hi</h1>
+    </div>
+  );
+}
