42d438293fc90ff662f3ace7d46a4bdf6a652998
Hugo <gitjohan5@gmail.com>
2024-06-03 22:18:04 +0300

    home page + nav update

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,78 @@
+"use client";
+
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [isHoveredGithub, setIsHoveredGithub] = useState(false);
+  const [isHoveredInstagram, setIsHoveredInstagram] = useState(false);
+
+  const handleMouseEnterGithub = () => {
+    setIsHoveredGithub(true);
+  };
+
+  const handleMouseLeaveGithub = () => {
+    setIsHoveredGithub(false);
+  };
+
+  const handleMouseEnterInstagram = () => {
+    setIsHoveredInstagram(true);
+  };
+
+  const handleMouseLeaveInstagram = () => {
+    setIsHoveredInstagram(false);
+  };
+
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center text-center gap-2">
+      <div className="flex min-h-screen flex-col items-center justify-center text-center gap-2 select-none">
         <p className="text-2xl xl:text-3xl font-semibold">
           Hello, It&apos;s me
         </p>
-        <h1 className="text-5xl xl:text-6xl font-bold">Hugo - HJ</h1>
+        <div className="relative">
+          <h1 className="text-5xl xl:text-6xl font-bold relative z-10">
+            Hugo - HJ
+          </h1>
+          <h1 className="text-5xl xl:text-6xl font-bold absolute top-1 left-1 text-[#FF5733]/80 z-0 w-full">
+            Hugo - HJ
+          </h1>
+        </div>
         <p className="max-w-sm md:max-w-lg text-lg mt-4">
           I am a fullstack focused webdeveloper. I design, construct, optimize
           and develop web apps and websites
         </p>
-        <div className="flex gap-12 mt-2">
-          <Link href="">
+        <div className="flex gap-12 mt-2 relative">
+          <Link
+            href="https://github.com/hugoqdesh"
+            onMouseEnter={handleMouseEnterGithub}
+            onMouseLeave={handleMouseLeaveGithub}
+            className="relative flex flex-col items-center"
+          >
             <FaGithub size={36} />
+            {isHoveredGithub && (
+              <p className="absolute top-full mt-2 bg-black/20 rounded p-2">
+                Github
+              </p>
+            )}
           </Link>
-          <Link href="">
+          <Link
+            href="https://www.instagram.com/hugoqdesh"
+            onMouseEnter={handleMouseEnterInstagram}
+            onMouseLeave={handleMouseLeaveInstagram}
+            className="relative flex flex-col items-center"
+          >
             <FaInstagram size={36} />
+            {isHoveredInstagram && (
+              <p className="absolute top-full mt-2 bg-black/20 rounded p-2">
+                Instagram
+              </p>
+            )}
           </Link>
         </div>
-        <p className="mt-4 bg-black/20 rounded p-2">discord: hugoqdesh</p>
+        <p className="fixed bottom-5 bg-[black]/20 rounded p-2 select-text">
+          discord: hugoqdesh
+        </p>
       </div>
     </>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,42 @@ import { RiMenu5Fill } from "react-icons/ri";
 const Header = () => {
   return (
     <nav className="flex items-center justify-between max-w-sm md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl px-4 md:px-0 mx-auto mt-4">
-      <div className="text-2xl lg:text-3xl font-bold">
-        <p>HJ</p>
+      <div className="font-bold">
+        <Link href="/" className="text-2xl group">
+          <span className="text-black group-hover:text-[#FF5733] transition duration-300">
+            H
+          </span>
+          <span className="text-[#FF5733] group-hover:text-black transition duration-300">
+            J
+          </span>
+        </Link>
       </div>
 
-      <div className="gap-6 text-lg hidden md:flex">
-        <Link href="">About</Link>
-        <Link href="">Projects</Link>
-        <Link href="">Contact</Link>
+      <div className="gap-6 text-lg hidden md:flex text-black/60">
+        <Link
+          href=""
+          className="hover:text-[#FF5733] font-semibold transition duration-300"
+        >
+          Home
+        </Link>
+        <Link
+          href=""
+          className="hover:text-[#FF5733] font-semibold transition duration-300"
+        >
+          About
+        </Link>
+        <Link
+          href=""
+          className="hover:text-[#FF5733] font-semibold transition duration-300"
+        >
+          Projects
+        </Link>
+        <Link
+          href=""
+          className="hover:text-[#FF5733] font-semibold transition duration-300"
+        >
+          Contact
+        </Link>
       </div>
 
       <div className="md:hidden">
