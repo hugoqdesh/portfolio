ee264e01ed652a8014e9cd303c8106ac88ea77d7
Hugo <gitjohan5@gmail.com>
2024-06-04 12:38:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,14 @@ export default function Home() {
 
   return (
     <>
+      <div className="fixed inset-0 flex justify-center h-8 mt-20 select-none">
+        <div className="border-2 border-[#FF5733]/70 rounded p-4 flex items-center bg-white">
+          <p>
+            <span className="text-[#FF5733]">!</span> This Portfolio is under
+            development <span className="text-[#FF5733]">!</span>
+          </p>
+        </div>
+      </div>
       <div className="flex min-h-screen flex-col items-center justify-center text-center gap-2 select-none">
         <p className="text-2xl xl:text-3xl font-semibold">
           Hello, It&apos;s me
