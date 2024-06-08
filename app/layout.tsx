c4b9f3a638da76599f50d22d4d0e83ae1670b333
Hugo <gitjohan5@gmail.com>
2024-06-08 11:27:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://hugoqdesh.vercel.app/"),
-  title: "HJ Portfolio",
+  title: "HJ Portfolio | Web Developer",
   description:
     "HJ's portfolio, Im a 16 year old full stack web developer who loves to build things with code.",
   openGraph: {
-    title: "HJ Portfolio",
+    title: "HJ Portfolio | Web Developer",
     description:
       "HJ's portfolio, Im a 16 year old full stack web developer who loves to build things with code.",
     type: "website",
