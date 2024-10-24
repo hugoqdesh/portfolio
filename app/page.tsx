6ff1475abc29224f2d581eacd95a17de41c24097
HJ <gitjohan5@gmail.com>
2024-10-24 18:23:40 +0300

    new portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,122 @@
-"use client";
-
 import Link from "next/link";
-import Image from "next/image";
-import { Github, Instagram } from "lucide-react";
+import {
+  MapPin,
+  BriefcaseBusiness,
+  ArrowRight,
+  MoveUpRight,
+  Mail,
+  Twitter,
+  Github,
+  Instagram,
+} from "lucide-react";
+import { Background } from "@/components/background";
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto py-16 px-4">
-      <header className="mb-8">
-        <div className="flex flex-col justify-center items-center">
-          <div className="w-24 h-24 rounded-full overflow-hidden flex items-center justify-center">
-            <Image
-              src="/me.jpg"
-              alt=""
-              width={1000}
-              height={1000}
-              className="object-cover select-none"
-            />
+    <Background>
+      <main>
+        <section className="max-w-2xl mx-auto py-20 px-4 md:px-0 flex flex-col gap-16 text-white/80">
+          <div>
+            <h1 className="text-xl pb-4 text-white/90 tracking-wide">
+              hugoqdesh
+            </h1>
+            <div className="flex flex-row gap-6 pb-4">
+              <p className="flex items-center gap-1">
+                <MapPin size={18} /> Estonia
+              </p>
+              <p className="flex items-center gap-1">
+                <BriefcaseBusiness size={18} /> Developer
+              </p>
+            </div>
+            <p>
+              Im a 17 y/o full stack web developer while also learning game
+              development and cybersecurity. Besides coding all day i enjoy
+              training Judo and Muay Thai.
+            </p>
           </div>
-          <h1 className="text-2xl mt-3">hugoqdesh</h1>
-        </div>
-      </header>
 
-      <section className="mb-8 text-center">
-        <p className="text-lg">
-          I&apos;m a 17 year old self-taught web developer from Estonia and
-          getting into cybersecurity. Currently working on{" "}
-          <Link
-            href="https://demo-buildup.vercel.app/"
-            target="_blank"
-            className="text-[#ed4160] font-semibold"
-          >
-            BuildUp
-          </Link>{" "}
-        </p>
-        <br />
-        <p className="text-lg">
-          My primary focus is developing full stack web applications and apps
-          that are meaningful for the users. Besides coding i train Judo and
-          Muay Thai, which help me stay focused and disciplined.
-        </p>
-        <br />
-        <p className="text-lg">
-          Looking to hire me? My Discord is{" "}
-          <span className="font-semibold">@hugoqdesh</span>
-        </p>
-      </section>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="flex flex-col gap-4">
+              <p className="text-sm">Building</p>
 
-      <footer className="flex flex-col md:flex-row justify-center gap-4 group">
-        <Link
-          href="https://github.com/hugoqdesh"
-          target="_blank"
-          className="flex gap-1 items-center text-black/50 hover:text-black transition duration-100"
-        >
-          {" "}
-          <Github size={18} /> github
-        </Link>
-        <Link
-          href="https://www.instagram.com/hugoqdesh/"
-          target="_blank"
-          className="flex gap-1 items-center text-black/50 hover:text-black transition duration-100"
-        >
-          {" "}
-          <Instagram size={18} /> instagram
-        </Link>
-      </footer>
-    </main>
+              <p>Currently not building anything.</p>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <p className="text-sm">Projects</p>
+
+              <div>
+                <Link
+                  href="https://www.hugoqdesh.lol/"
+                  className="border-b-2 border-white/10 w-max flex items-center gap-1 text-white/90"
+                >
+                  Portfolio
+                </Link>
+                <p className="mt-1">Your on here rn. 😊</p>
+              </div>
+
+              <div>
+                <Link
+                  href="https://www.digestify.dev/"
+                  className="border-b-2 border-white/10 w-max flex items-center gap-1 text-white/90"
+                >
+                  Digestify
+                </Link>
+                <p className="mt-1">
+                  Hashing, encryption, key generation, and more.
+                </p>
+
+                <Link
+                  href="https://github.com/hugoqdesh"
+                  className="flex items-center gap-0.5 border-b-2 border-white/10 w-max mt-6"
+                >
+                  All projects <ArrowRight size={17} />
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h1 className="text-xl pb-6 text-white/90">connect</h1>
+            <div className="flex flex-col md:flex-row gap-8">
+              <Link
+                href="mailto:gitjohan5@gmail.com"
+                className="flex items-center gap-2 hover:bg-white/5 p-2 rounded hover:text-white transition duration-200"
+              >
+                <Mail size={18} /> Email <MoveUpRight size={16} />
+              </Link>
+              <Link
+                href="https://x.com/hugoqdesh"
+                className="flex items-center gap-2 hover:bg-white/5 p-2 rounded hover:text-white transition duration-200"
+              >
+                <Twitter size={18} /> X.com <MoveUpRight size={16} />
+              </Link>
+              <Link
+                href="https://github.com/hugoqdesh"
+                className="flex items-center gap-2 hover:bg-white/5 p-2 rounded hover:text-white transition duration-200"
+              >
+                <Github size={18} /> Github <MoveUpRight size={16} />
+              </Link>
+              <Link
+                href="https://www.instagram.com/hugoqdesh"
+                className="flex items-center gap-2 hover:bg-white/5 p-2 rounded hover:text-white transition duration-200"
+              >
+                <Instagram size={18} /> Instagram <MoveUpRight size={16} />
+              </Link>
+            </div>
+            <p className="mt-12">
+              Or add me on discord <span className="italic">@hugoqdesh</span>
+            </p>
+          </div>
+        </section>
+
+        <footer className="border-t border-white/10 mt-14">
+          <div className="max-w-2xl px-4 md:px-0 mx-auto flex justify-between py-6 text-sm text-white/50">
+            <p>Shine, constantly and steadily.</p>
+            <p>2024</p>
+          </div>
+        </footer>
+      </main>
+    </Background>
   );
 }
