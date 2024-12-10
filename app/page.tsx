5932d9ed50416b21ae956eab4de22ba3258f5758
HJ <gitjohan5@gmail.com>
2024-12-10 17:58:19 +0200

    remaking my portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { Profile } from "@/components/Profile";
-import { Projects } from "@/components/Projects";
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center max-w-lg mx-auto py-12 px-4 sm:py-24 sm:px-0">
-      <Profile />
-      <Projects />
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="space-y-4 text-center max-w-xl px-4">
+        <h1 className="text-3xl font-light tracking-wide">
+          Remaking my portfolio
+        </h1>
+        <p className="text-base text-neutral-500 leading-relaxed">
+          Done in a few days
+        </p>
+      </div>
     </main>
   );
 }
